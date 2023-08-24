72d971cca6b44ab312ccaccdc98dcb5b4088e2fd
Murzabulatov <slvtz@icloud.com>
2023-08-24 17:54:21 +0000

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import {TagModule} from "./tag/tag.module";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import ormconfig1 from '@app/ormconfig';
+import {UserModule} from "@app/user/user.module";
+
+@Module({
+  imports: [TypeOrmModule.forRoot(ormconfig1), TagModule, UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/ormconfig.ts
@@ -0,0 +1,15 @@
+import {ConnectionOptions} from "typeorm";
+
+const config: ConnectionOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'mediumclone',
+    password: '123',
+    database: 'mediumclone',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: false,
+    migrations: [__dirname + '/migrations/**/*{.ts,.js}']
+}
+
+export default config
